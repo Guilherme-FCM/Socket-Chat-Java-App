591223f53dc41d83ccfdc9e2545326585d2785e3
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-09-05 21:42:16 -0300

    create getInetAddress method

--- src/SocketClient.java
+++ src/SocketClient.java
@@ -2,13 +2,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class SocketClient {
     private final BufferedReader in;
     private final PrintWriter out;
+    private final Socket socket;
 
     public SocketClient(Socket socket) {
+        this.socket = socket;
+
         try {
             this.in = new BufferedReader(
                 new InputStreamReader(socket.getInputStream())
@@ -30,5 +34,9 @@ public class SocketClient {
         } catch (IOException e) {
             return "";
         }
+    }
+
+    public InetAddress getInetAddress(){
+        return socket.getInetAddress();
     }
 }
