fde17b386d7ccfbefc9b21b78b255e62d63127cb
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-09-05 21:13:56 -0300

    Create SocketClient

---
+++ src/SocketClient.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SocketClient {
+    private final BufferedReader in;
+    private final PrintWriter out;
+
+    public SocketClient(Socket socket) {
+        try {
+            this.in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream())
+            );
+            this.out = new PrintWriter(socket.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean sendMessage(String message){
+        this.out.println(message);
+        return !out.checkError();
+    }
+
+    public String getMessage(){
+        try {
+            return in.readLine();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+}
