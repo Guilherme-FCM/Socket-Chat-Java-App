e76c9537e9b1bd9d037b2e3973c54e94f0beb153
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-09-05 21:11:38 -0300

    Add start method on Client

--- src/Client.java
+++ src/Client.java
@@ -1,12 +1,22 @@
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     private static final String HOST = "localhost";
     private Socket socket;
+    private Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        new Client();
+        new Client().start();
+    }
+
+    private void start() {
+        String message;
+        do {
+            System.out.print("Type a message: ");
+            message = scanner.nextLine();
+        } while( !message.equalsIgnoreCase("sair") );
     }
 
     public Client() {
