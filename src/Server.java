091fcf6d6d80969d3d53bc157283171a24e23115
Guilherme-FCM <guilherme_fcm@hotmail.com>
2022-09-08 09:53:54 -0300

    Send messages received to all clients

--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,12 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Server {
     public static final int PORT = 8000;
     private ServerSocket serverSocket;
+    private List<SocketClient> clients = new LinkedList<>();
 
     public static void main(String[] args) {
         new Server().start();
@@ -14,21 +17,28 @@ public class Server {
         try {
             while (true) {
                 SocketClient socketClient = new SocketClient(serverSocket.accept());
+                clients.add(socketClient);
                 System.out.println("Client " + socketClient.getInetAddress() + " connected.");
 
-                new Thread(() -> messageLoop(socketClient)).start();
+                new Thread(() -> this.messageLoop(socketClient)).start();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void messageLoop(SocketClient socketClient) {
+    private void messageLoop(SocketClient socketClient) {
         String message = "";
         do {
             message = socketClient.getMessage();
             System.out.println("Client " + socketClient.getInetAddress() + ": " + message);
+            this.sendMessageToClients(message);
         } while (!message.isEmpty());
+    }
+
+    private void sendMessageToClients(String message){
+        for (SocketClient client : clients)
+            client.sendMessage(message);
     }
 
     public Server() {
